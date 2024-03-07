23ed31239cbe0fa1b56c1709f6d19908b000aab8
ohazutmelicsohn <ohazutmelicsohn@varonis.com>
2024-03-07 09:43:59 +0200

    Updated Angular Interview Blank project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,37 @@
+import { CommonModule } from '@angular/common';
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
 
 @Component({
+  standalone: true,
+  imports: [CommonModule],
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  templateUrl: 'app.component.html',
 })
 export class AppComponent {
-  title = 'angular-second-interview';
+  // TODOS API - https://dummyjson.com/docs/todos
+
+  todos!: any[];
+
+  constructor(private http: HttpClient) {}
+
+  ngOnInit() {
+    this.http.get<any[]>('https://dummyjson.com/todos').subscribe((todos) => {
+      this.todos = todos;
+    });
+  }
+
+  add(todo) {
+    this.http
+      .post<any[]>('https://dummyjson.com/todos/add', {})
+      .subscribe((todo) => {
+        this.todos.unshift(todo);
+      });
+  }
+
+  // TODO: Implement a delete TODO action
+  delete() {}
+
+  // TODO: Implement an update TODO action
+  editTodo() {}
 }
